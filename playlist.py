912b089f5d024c2955889273c59bb63ce17c6118
amandapickering <pickering.amanda@gmail.com>
2014-12-11 16:35:01 -0500

    gunicorn?

--- playlist.py
+++ playlist.py
@@ -68,6 +68,6 @@ def index():
   		return render_template('index.html', songids=user_songs)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
